5f13ca95118eff6f30030de538fa07ebced655f5
Sean Nekouei <sean.nekouei@gmail.com>
2023-02-27 11:45:04 -0700

    commit tree a7a4faabfbda52800ef7b0a62173ad7b69f02677

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+const scubers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(){
+    return scubers.slice(0, 2)
+}
+const returnLastTwoDrivers = function() {
+    return scubers.slice(-2)  
+}
+const selectingDrivers = function() {
+ returnFirstTwoDrivers()
+  
+}
